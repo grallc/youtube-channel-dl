dfe736b84c3390cd11b7acacbb92ace13683162c
Corentin Grall <corentin.grall@gmail.com>
2019-05-09 11:07:21 +0200

    Added depedencies :heavy_plus_sign:

---
+++ app.js
@@ -0,0 +1,11 @@
+const axios = require('axios');
+const fs = require('fs');
+const ytdl = require('youtube-dl');
+
+const channelLink = `https://www.youtube.com/channel/UCWrW3kmk_mpt4H86OBhleaA/videos`; 
+
+const downloadVideos = (youtubeChannelLink) => {
+
+}
+
+
