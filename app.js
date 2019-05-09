f7c0b586e2a890d546f1f5ee37d61a5c0bf3d15b
Corentin Grall <corentin.grall@gmail.com>
2019-05-09 11:15:42 +0200

    We can now list all a youtube channel's videos :sparkles:

--- app.js
+++ app.js
@@ -2,10 +2,26 @@ const axios = require('axios');
 const fs = require('fs');
 const ytdl = require('youtube-dl');
 
-const channelLink = `https://www.youtube.com/channel/UCWrW3kmk_mpt4H86OBhleaA/videos`; 
+let channelLink = `https://www.youtube.com/channel/UCWrW3kmk_mpt4H86OBhleaA/`;
+channelLink = channelLink.replace(`https://www.youtube.com/channel/`, ``).replace(`/`, ``)
 
-const downloadVideos = (youtubeChannelLink) => {
-
+const getVideos = async (youtubeChannelLink) => {
+  try {
+    const response = await axios.get(`https://www.googleapis.com/youtube/v3/search?channelId=${channelLink}&part=snippet,id&order=date&maxResults=20&key=${process.env.GOOGLE_API_KEY}`);
+    return response.data;
+  } catch (e) {
+    throw new Error(`Unable to get this channel's videos.`);
+  }
 }
 
 
+
+const downloadVideos = async (youtubeChannelLink) => {
+  
+}
+
+getVideos(channelLink).then((response) => {
+  console.log(response);
+}).catch((e) => {
+  console.log(e.message);
+})
