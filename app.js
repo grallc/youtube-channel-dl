858030d7e9547eab91d1a354fc554e5282ec6cd9
Corentin Grall <corentin.grall@gmail.com>
2019-05-09 11:25:45 +0200

    Video ID are now listed :construction:

--- app.js
+++ app.js
@@ -7,21 +7,20 @@ channelLink = channelLink.replace(`https://www.youtube.com/channel/`, ``).replac
 
 const getVideos = async (youtubeChannelLink) => {
   try {
-    const response = await axios.get(`https://www.googleapis.com/youtube/v3/search?channelId=${channelLink}&part=snippet,id&order=date&maxResults=20&key=${process.env.GOOGLE_API_KEY}`);
+    const response = await axios.get(`https://www.googleapis.com/youtube/v3/search?channelId=${channelLink}&part=snippet,id&order=date&maxResults=50&key=${process.env.GOOGLE_API_KEY}`);
     return response.data;
   } catch (e) {
     throw new Error(`Unable to get this channel's videos.`);
   }
 }
 
-
-
 const downloadVideos = async (youtubeChannelLink) => {
-  
+  const videos = await getVideos(youtubeChannelLink);
+  for(let x = 0; x<videos.items.length; x++) {
+    if(videos.items[x].id.kind === 'youtube#video') {
+      console.log(videos.items[x].id.videoId);
+    }
+  }
 }
 
-getVideos(channelLink).then((response) => {
-  console.log(response);
-}).catch((e) => {
-  console.log(e.message);
-})
+downloadVideos(channelLink);
